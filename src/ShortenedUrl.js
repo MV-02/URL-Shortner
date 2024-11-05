e1bc11406d38083a818fafc9b4ac88d6f7339811
mv <mehul25169@gmail.com>
2024-11-05 16:56:48 +0530

    added toast feature when copied

--- src/ShortenedUrl.js
+++ src/ShortenedUrl.js
@@ -1,25 +1,39 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ShortenedUrl = () => {
   const [shortenLink, setShortenLink] = useState("sample");
   const [copied, setCopied] = useState(false);
+  const notify = () => {
+    toast.success("Copied!", {
+      position: "bottom-center",
+      autoClose: 1000,
+    });
+  };
 
   useEffect(() => {
-    const timer=setTimeout(() => {
+    const timer = setTimeout(() => {
       setCopied(false);
     }, 1000);
-  
-    return () => clearTimeout(timer);
-  }, [copied])
-  
 
+    return () => clearTimeout(timer);
+  }, [copied]);
   return (
     <div className="ResultContainer">
       <p>{shortenLink}</p>
-      <CopyToClipboard text={shortenLink} onCopy={() => setCopied(true)}>
-        <button className= {copied?"copied":""}>Copy</button>
+      <CopyToClipboard
+        text={shortenLink}
+        onCopy={() => {
+          setCopied(true);
+        }}
+      >
+        <button onClick={notify} className={copied ? "copied" : ""}>
+          Copy
+        </button>
       </CopyToClipboard>
+      <ToastContainer />
     </div>
   );
 };
